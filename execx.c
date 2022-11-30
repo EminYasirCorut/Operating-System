eb68c8993992b1e131ef8dc195077c571d6092e1
EminYasirCorut <115791281+EminYasirCorut@users.noreply.github.com>
2022-11-30 16:52:08 +0300

    Add files via upload

--- execx.c
+++ execx.c
@@ -9,6 +9,39 @@ int main(int argc, char *argv[], char** envp){
     
     int numberOfTurns= atoi(argv[1]);//kac kere tekrarlanacak
     
+    if (strcmp("ls",argv[2])== 0)
+    {
+        for(int i=0;i<numberOfTurns;i++){
+            int pid, status;
+            pid = fork();
+            if (pid > 0) {
+                    wait(&status);
+            } else {
+                status=system("ls");
+                exit(1);
+            }
+        }
+        printf("myshell>> ");
+        
+    }
+
+    if (strcmp("clear",argv[2])== 0)
+    {
+        for(int i=0;i<numberOfTurns;i++){
+            int pid, status;
+            pid = fork();
+            if (pid > 0) {
+                    wait(&status);
+            } else {
+                status=system("clear");
+                exit(1);
+            }
+        }
+        printf("myshell>> ");
+        
+    }
+    
+    
     if(strcmp("writef",argv[2]) == 0){
         char* newargv[3];
         newargv[0]=argv[3];
